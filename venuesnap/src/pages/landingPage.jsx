0f05fba2c6d05cc6519f37cc89bdca9151d333b8
Vanessa Rice <vanessa.rice@keyin.com>
2024-08-08 18:16:54 -0230

    added landingpage.

---
+++ venuesnap/src/pages/landingPage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import useFetchData from "../hooks/useFetchData"; 
+
+const LandingPage = () => {
+  const { data: events, error, loading } = useFetchData("/events");
+
+  if (loading) return <p>Loading events...</p>;
+  if (error) return <p>Error loading events: {error.message}</p>;
+
+  return (
+    <div>
+      <h1>Events</h1>
+      {events && events.length > 0 ? (
+        <ul>
+          {events.map((event) => (
+            <li key={event.id}>
+              <h2>{event.name}</h2>
+              <p>{event.venueId}</p>
+              <p>Date: {event.date}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No events found.</p>
+      )}
+    </div>
+  );
+};
+
+export default LandingPage;
